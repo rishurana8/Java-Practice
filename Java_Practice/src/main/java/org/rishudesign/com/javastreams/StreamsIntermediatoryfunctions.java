fa847edb13455ccdcc4affc5ba1f80f2fa46e9c0
rishurana8 <rishurana256@gmail.com>
2025-08-05 16:41:15 +0530

    intermediatory functions

--- Java_Practice/src/main/java/org/rishudesign/com/javastreams/StreamsIntermediatoryfunctions.java
+++ Java_Practice/src/main/java/org/rishudesign/com/javastreams/StreamsIntermediatoryfunctions.java
@@ -1,6 +1,7 @@
 package org.rishudesign.com.javastreams;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamsIntermediatoryfunctions {
@@ -29,6 +30,50 @@ public class StreamsIntermediatoryfunctions {
         for(String name: smallercaseletternames){
             System.out.println(name);
         }
+
+        // flatmap
+        List<List<String>> sentenceList = Arrays.asList(
+                Arrays.asList("I","LOVE","CODING"),
+                Arrays.asList("I","LOVE","JAVA"),
+                Arrays.asList("THIS","IS","GREAT")
+        );
+
+        List<String> flatlist = sentenceList
+                .stream()
+                .flatMap((List<String>sentence) ->sentence.stream())
+                .collect(Collectors.toList());
+
+        for(String list: flatlist){
+            System.out.println(list);
+        }
+
+        // distinct
+        Integer[] arr = {1,5,3,3,2,4,2};
+        Stream<Integer> arrStream = Arrays
+                .stream(arr)
+                .distinct();
+
+        for(Integer ele: arr){
+            System.out.println(ele);
+        }
+
+        //sorted
+        Integer[] arr2 = {1,4,10,3,9,8,6};
+        Stream<Integer> integerStream = Arrays.stream(arr2).sorted();
+
+        for(Integer ele: arr2){
+            System.out.println(ele);
+        }
+
+        // sorting with comparator
+        Integer[] arr3 = {1,2,4,5,6,7,8,9};
+        Stream<Integer> desstream = Arrays.stream(arr3).sorted((Integer val1,Integer val2) -> val2-val1);  // this will generate a stream which will sort them in descending order
+
+        //mapToInt
+        List<String> numbers = Arrays.asList("1","2","3","4");
+        IntStream streams = numbers.stream().mapToInt((String val) -> Integer.parseInt(val));
+        int [] nums = streams.toArray();
+        // here we cannnot do List<int> because always work in Integer wrapper classes
 
     }
 }
